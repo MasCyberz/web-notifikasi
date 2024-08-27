0ac3ff4467fceb7493e534de1b877395dfc8ed4c
MasCyberz <dimasned20@gmail.com>
2024-08-27 15:25:46 +0700

    revisi form kir

--- resources/views/kir/add.blade.php
+++ resources/views/kir/add.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <x-PageHeader header="Data STNK" classcontainer="col-lg-8"/>
+    <x-PageHeader header="Data KIR" classcontainer="col-lg-8"/>
     <div class="page-body">
 
 
         <div class="col-12 col-lg-8 container-xl">
-            {{-- Form Create STNK --}}
+            {{-- Form Create KIR --}}
             <form action="" class="card">
                 <x-cardHeader titleHeader="Silahkan isi data dibawah ini dengan benar!"/>
                 <div class="card-body">
@@ -12,18 +12,13 @@
                         <label class="form-label">Kendaraan</label>
                         <div>
                             <select class="form-select w-25">
-                                <option>Mobil</option>
-                                <option>Truck</option>
-                                <option>Motor</option>
+                                <option>Wuling Cortez | B 1234 XYZ</option>
+                                <option>Wuling Cortez | B 1234 XYZ</option>
+                                <option>Wuling Cortez | B 1234 XYZ</option>
                             </select>
                         </div>
                     </div>
                     <x-Input label="Tgl. Perpanjangan KIR" name="tanggal_perpanjangan" type="date" class="w-25"/>
-                    <div class="mb-3">
-                        <label class="form-label">Plat Nomor</label>
-                        <input type="text" name="plat_nomor" class="form-control" data-mask="** 0000 ***"
-                            data-mask-visible="true" placeholder="B 1234 XYZ" autocomplete="off">
-                    </div>
                 </div>
                 <x-cardFooter route="{{ route('stnk-index') }}"/>
             </form>
